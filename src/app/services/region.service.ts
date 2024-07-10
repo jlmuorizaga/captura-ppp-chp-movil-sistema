576c221f746ac97e1c7e25a453c4fef61163ff41
jlmuorizaga <61767859+jlmuorizaga@users.noreply.github.com>
2024-07-09 20:45:41 -0600

    Add environment

--- src/app/services/region.service.ts
+++ src/app/services/region.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,13 @@ export class RegionService {
   constructor(private http:HttpClient) { }
 
   dameListaSucursales(){
-    return this.http.get('http://ec2-54-153-58-93.us-west-1.compute.amazonaws.com:3005/regiones');
+    //return this.http.get('http://ec2-54-153-58-93.us-west-1.compute.amazonaws.com:3005/regiones');
+    return this.http.get(environment.baseUrl+':'+environment.puertoApiAdmonCatalogos+environment.regiones);
+
   }
   borraRegion(id:string){
-    return this.http.delete('http://ec2-54-153-58-93.us-west-1.compute.amazonaws.com:3005/regiones/'+id);
+    //return this.http.delete('http://ec2-54-153-58-93.us-west-1.compute.amazonaws.com:3005/regiones/'+id);
+    return this.http.delete(environment.baseUrl+':'+environment.puertoApiAdmonCatalogos+environment.regiones+'/'+id);
 
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl:'http://ec2-54-153-58-93.us-west-1.compute.amazonaws.com',
+  puertoApiAdmonCatalogos:'3005',
+  regiones:'/regiones',
+
 };
