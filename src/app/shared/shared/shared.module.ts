a13aecf5b6bfe19061f8c4248bb148f8a350466d
jlmuorizaga <jlmuorizaga@gmail.com>
2024-07-19 19:49:44 -0600

    Update shared.module.ts

--- src/app/shared/shared/shared.module.ts
+++ src/app/shared/shared/shared.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegionService } from 'src/app/services/region.service';
-import { HttpClientModule } from '@angular/common/http';
 import { EspecialidadService } from 'src/app/services/especialidad.service';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    HttpClientModule
+    CommonModule,HttpClientModule
   ],
   providers:[RegionService,EspecialidadService]
 })
